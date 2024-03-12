87213c7fa8b26e67586b46d9b9cca0582a5553fb
Kartikey Garg <kartikey.garg@studentambassadors.com>
2024-03-12 05:35:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Kartikey Garg",
+  title: "Devrel & Community Manager",
+  email: "Kartikeygarg2004@gmail.com",
+  gitHub: "geekykartikey",
+  instagram: "geeky_kartikey",
+  linkedIn: "geekykARTIKEY",
   medium: "",
-  twitter: "microsoft",
+  twitter: "geeky_kartikey",
   youTube: "Code",
 };
 
